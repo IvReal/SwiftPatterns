bb59c9beb680a6adcf1fe20b0cdf89bae5f2cc63
IvReal <ivreal@yandex.ru>
2019-10-15 19:37:05 +0300

    Initial commit

---
+++ SwiftPatterns/SwiftPatterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPatterns
+//
+//  Created by Iv on 15.10.2019.
+//  Copyright © 2019 Iv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
